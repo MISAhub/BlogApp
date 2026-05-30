5ef53530a9daade399054fd589f1fb0aeee12c58
MISAhub <milind.sawai09@gmail.com>
2026-05-30 21:48:55 +0530

    Second

--- server.js
+++ server.js
@@ -3,9 +3,27 @@ const port = process.env.PORT || 3000;
 const path = require('path');
 const app = express();
 
+require('dotenv').config();
+const cookies = require('cookie-parser');
+
+
 // ✅ THIS was missing — serves everything in /public as static files
 // This is what allows fetch('/components/nav.html') to work
+
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public', 'components'))); // Serve components as static files
+app.use(cookies());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// app.use('/api', require('./routes/api'));
+// app.use('/auth', require('./routes/auth'));
+// app.use('/users', require('./routes/users'));
+
+app.use('/api/auth', require('./src/routes/auth.js'));
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "public", "home.html"), (err) => {
